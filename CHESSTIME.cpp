ebf90305ef1068919c3f9e6dc6240a837d4ebde4
Habibullah Bahar <69121827+iHabibullahBahar@users.noreply.github.com>
2022-07-14 23:28:44 +0600

    Create CHESSTIME.cpp

---
+++ CHESSTIME.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        n*=60;
+        cout<<n/20<<endl;
+    } 
+}
